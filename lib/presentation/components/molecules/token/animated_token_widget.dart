1d9c4ec610fe731b5bab068a1b2aad34371750ff
Victor Hugo <victor.030farias@gmail.com>
2025-04-14 21:40:02 -0300

    feat: :sparkles: Adiciona rotação à ficha

---
+++ lib/presentation/components/molecules/token/animated_token_widget.dart
@@ -0,0 +1,57 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:mtg_token_manager/presentation/components/molecules/token/helper/token.dart';
+import 'package:mtg_token_manager/presentation/components/molecules/token/token_widget.dart';
+
+class AnimatedTokenWidget extends StatefulWidget {
+  const AnimatedTokenWidget({super.key});
+
+  @override
+  State<AnimatedTokenWidget> createState() => _AnimatedTokenWidgetState();
+}
+
+class _AnimatedTokenWidgetState extends State<AnimatedTokenWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool rotated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(microseconds: 400),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: math.pi / 2,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  void _rotate() {
+    if (rotated) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
+
+    rotated = !rotated;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _rotate,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.rotate(angle: _animation.value, child: child);
+        },
+        child: TokenWidget(token: Token.faerie()),
+      ),
+    );
+  }
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mtg_token_manager/presentation/components/molecules/token/helper/token.dart';
-import 'package:mtg_token_manager/presentation/components/molecules/token/token_widget.dart';
+import 'package:mtg_token_manager/presentation/components/molecules/token/animated_token_widget.dart';
+// import 'package:mtg_token_manager/presentation/components/molecules/token/helper/token.dart';
+// import 'package:mtg_token_manager/presentation/components/molecules/token/token_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: TokenWidget(token: Token.faerie())));
+    return const Scaffold(body: Center(child: AnimatedTokenWidget()));
   }
 }
